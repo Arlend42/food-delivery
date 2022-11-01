1116b71e2d596082fd0f71af555bf3abcb019cad
Arlend Lela <arlendlela@gmail.com>
2022-11-01 11:53:30 +0100

    Adding bootstrap and styles to the page

--- main/settings.py
+++ main/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -79,11 +79,11 @@ WSGI_APPLICATION = "main.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME":  "postgres",
-        'USER': 'mac',
-        'PASSWORD': 'Eldi 2005',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        "NAME": "postgres",
+        "USER": "mac",
+        "PASSWORD": "Eldi 2005",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = ['main/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
