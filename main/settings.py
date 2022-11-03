b57edc710a94651585b8eaf9b93aa408f45575cc
Arlend Lela <arlendlela@gmail.com>
2022-11-03 14:12:58 +0100

    Added a feature when the user is created automatically creates user profile. Added some changes into the templates

--- main/settings.py
+++ main/settings.py
@@ -127,8 +127,11 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = ["main/static"]
 
+#  media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
