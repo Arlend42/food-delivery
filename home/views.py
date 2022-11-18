0de8253d81d36888ec701ae4e4066ec28d86e548
Arlend Lela <arlendlela@gmail.com>
2022-11-18 10:24:36 +0100

    Add top restaurants in the homepage

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 
 from django.shortcuts import render
 # from django.http import HttpResponse
+from restaurants.models import Restaurant
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "home.html")
+    restaurants = Restaurant.objects.filter(is_approved=True, user__is_active=True)[:8]
+    print(restaurants)
+    context = {
+        'restaurants': restaurants,
+    }
+    return render(request, "home.html", context)
