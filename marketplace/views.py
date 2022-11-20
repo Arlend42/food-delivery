f4a258e4e9728fd3c3ae077bdbdd9b500e9bee11
Arlend Lela <arlendlela@gmail.com>
2022-11-20 01:38:26 +0100

    Search by keyword added

--- marketplace/views.py
+++ marketplace/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Prefetch
 from restaurants.models import Restaurant
 from menu.models import Category, Product
+from django.db.models import Q
 
 # Create your views here.
 
@@ -31,3 +32,19 @@ def restaurant_detail(request, restaurant_slug):
         'categories': categories,
     }
     return render(request, 'marketplace/restaurant_detail.html', context)
+
+
+def search(request):
+    keyword = request.GET['keyword']
+    # get the restaurant ids that has the product user is searching for
+    search_by_product = Product.objects.filter(food_name__icontains=keyword, is_available=True).values_list('restaurant', flat=True)
+    print(search_by_product)
+    restaurants = Restaurant.objects.filter(Q(id__in=search_by_product) | Q(restaurant_name__icontains=keyword, is_approved=True, user__is_active=True))
+    # restaurants = Restaurant.objects.filter(restaurant_name__icontains=keyword, is_approved=True, user__is_active=True)
+    restaurant_counter = restaurants.count()
+    print(restaurants)
+    context = {
+        'restaurants': restaurants,
+        'restaurant_counter': restaurant_counter
+    }
+    return render(request, 'marketplace/restaurant_list.html', context)
