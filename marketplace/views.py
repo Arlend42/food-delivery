77e3454d945a724a48ed3cb0a5164b9e79b59661
Arlend Lela <arlendlela@gmail.com>
2022-11-20 10:00:20 +0100

    added small changes

--- marketplace/views.py
+++ marketplace/views.py
@@ -42,7 +42,7 @@ def search(request):
     restaurants = Restaurant.objects.filter(Q(id__in=search_by_product) | Q(restaurant_name__icontains=keyword, is_approved=True, user__is_active=True))
     # restaurants = Restaurant.objects.filter(restaurant_name__icontains=keyword, is_approved=True, user__is_active=True)
     restaurant_counter = restaurants.count()
-    print(restaurants)
+    print(restaurants, restaurant_counter)
     context = {
         'restaurants': restaurants,
         'restaurant_counter': restaurant_counter
